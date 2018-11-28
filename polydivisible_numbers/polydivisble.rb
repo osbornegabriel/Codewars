6414f65ba1b000c378e9b220283e3572efa77d65
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-28 16:40:51 -0500

    Update convert_to_base_10 method to handle all bases up to 64

--- polydivisible_numbers/polydivisble.rb
+++ polydivisible_numbers/polydivisble.rb
@@ -8,9 +8,11 @@ def is_polydivisible?(s, b)
 end
 
 def get_polydivisible(n, b)
-  #your code here
+
 end
 
 def convert_to_base_10(s,b)
-  s.chars.reverse.map.with_index{|n,i| n.to_i * (b ** i)}.reduce(:+)
+  a = s.chars.reverse
+  a.map!{|l| CHARS.index(l)}
+  a.map.with_index{|n,i| n.to_i * (b ** i)}.reduce(:+)
 end
