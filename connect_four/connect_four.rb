52a0d8a0ccdb30527d1eb774ecccd8acee06ca95
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-29 15:17:49 -0500

    Write play_moves class method

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -9,12 +9,6 @@ class Connect_Four
     @board = create_board
   end
 
-  def drop_checker(play)
-    column = checker[0]
-    color = checker[2]
-    @board[column][@board[column].index('')] = color
-  end
-
   def create_board
     columns = ('A'..'G').to_a
     board = Hash.new
@@ -22,6 +16,28 @@ class Connect_Four
     board
   end
 
+  def drop_checker(checker)
+    column = checker[0]
+    color = checker[2]
+    @board[column][@board[column].index('')] = color
+  end
+
+  def play_checkers(moves)
+    moves.each{|move| drop_checker(move)}
+  end
+
 end
 
-p Connect_Four.new
+p board = Connect_Four.new
+# board.drop_checker("A_Red")
+# p board.board
+game =   ["A_Red",
+  "B_Yellow",
+  "A_Red",
+  "B_Yellow",
+  "A_Red",
+  "B_Yellow",
+  "G_Red",
+  "B_Yellow"]
+board.play_checkers(game)
+p board
