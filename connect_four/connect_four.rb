7460cc839b22b065ad006b1dba657b4509d239bc
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-29 15:03:35 -0500

    Begin Connect_Four class and create_board

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,3 +1,21 @@
 def who_is_winner(pieces_position_list)
   #le code
 end
+
+class Connect_Four
+  attr_accessor :board
+
+  def initialize
+    @board = create_board
+  end
+
+  def create_board
+    columns = ('A'..'G').to_a
+    board = Hash.new
+    columns.each {|column| board[column] = [''] * 6}
+    board
+  end
+
+end
+
+p Connect_Four.new.board
