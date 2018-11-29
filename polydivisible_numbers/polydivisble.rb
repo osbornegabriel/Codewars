f8daa0bde5c0bd78f4320b823c1a79dab3a42059
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-28 19:01:33 -0500

    up get_polydivisible method to incorporate convert_from_base_10

--- polydivisible_numbers/polydivisble.rb
+++ polydivisible_numbers/polydivisble.rb
@@ -8,19 +8,41 @@ def is_polydivisible?(s, b)
 end
 
 def get_polydivisible(n, b)
-  return 'radix issue' if b > 36
-  test = 0
+  b10_val = 0
   until n == 0 do
-    n -= 1 if is_polydivisible?(test.to_s(b),b)
-    return test.to_s(b).upcase if n == 0
-    test += 1
+    b_val = convert_from_base_10(b10_val,b)
+    n -= 1 if is_polydivisible(b_val,b)
+    return b_val if n == 0
+    b10_val += 1
   end
 end
+
+# def get_polydivisible(n, b)
+#   return 'radix issue' if b > 36
+#   test = 0
+#   until n == 0 do
+#     n -= 1 if is_polydivisible?(test.to_s(b),b)
+#     return test.to_s(b).upcase if n == 0
+#     test += 1
+#   end
+# end
 
 def convert_to_base_10(s,b)
   a = s.chars.reverse
   a.map!{|l| CHARS.index(l)}
   a.map.with_index{|n,i| n.to_i * (b ** i)}.reduce(:+)
+end
+
+def convert_from_base_10(i,b)
+  return i.to_s(b).upcase if b < 37
+  table = base_table(i,b)
+  solution = Array.new
+  table.each do |d|
+    value = i / d
+    solution.push(CHARS[value])
+    i %= d
+  end
+  solution.join
 end
 
 def base_table(i,b)
