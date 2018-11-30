3eee8b640f62cf7df6b75598a97738f2fa93268c
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-29 20:23:59 -0500

    Write visualize_gameboard for visual aid in debugging purposes

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -23,6 +23,7 @@ class Connect_Four
     game_sequence.each do |move|
       drop_checker(move)
       check_board
+      visualize_gameboard if @game_over
       break if @game_over
     end
     @winner
@@ -35,19 +36,20 @@ class Connect_Four
   end
 
   def check_lines(lines)
-    lines.each{|line| check_line(line)}
+    lines.each{|line| check_line(line); break if @game_over}
   end
 
   def check_diagonal
-    check_lines(first_column_start + two_to_four_column_start + four_to_six_column_start + last_column_start)
+    check_lines(first_column_start + two_to_four_column_start)
+    check_lines(four_to_six_column_start + last_column_start)
   end
 
   def rev_construct_diagonal(column_i,row_i)
-    columns = @board.values.reverse
+    columns = @board.values
     diagonal = []
-    until row_i > 5 || column_i > 6
+    until row_i > 5 || column_i < 0
       diagonal << columns[column_i][row_i]
-      column_i += 1
+      column_i -= 1
       row_i += 1
     end
     diagonal
@@ -82,17 +84,17 @@ class Connect_Four
 
   def four_to_six_column_start
     [
-      rev_construct_diagonal(1,0),
-      rev_construct_diagonal(2,0),
-      rev_construct_diagonal(3,0)
+      rev_construct_diagonal(3,0),
+      rev_construct_diagonal(4,0),
+      rev_construct_diagonal(5,0)
     ]
   end
 
   def last_column_start
     [
-      rev_construct_diagonal(0,0),
-      rev_construct_diagonal(0,1),
-      rev_construct_diagonal(0,2)
+      rev_construct_diagonal(6,0),
+      rev_construct_diagonal(6,1),
+      rev_construct_diagonal(6,2)
     ]
   end
 
@@ -107,7 +109,7 @@ class Connect_Four
   def check_line(line)
     four = line.join.scan(/(Red|Yellow)\1{3}/)
     @game_over = true if !!four[0]
-    @winner = four[0][0] if four[0] && !@winner
+    @winner = four[0][0] if four[0]
   end
 
   def check_row(row_index)
@@ -129,22 +131,27 @@ class Connect_Four
     @board[column][@board[column].index('')] = color
   end
 
+  #Fun visualization to aid potential debugging
+  def visualize_gameboard
+    @board.values.transpose.reverse.each{|row| p row}
+  end
+
 end
 
-p board = Connect_Four.new
-# board.drop_checker("A_Red")
-# p board.board
-game =   ["A_Red",
-  "B_Yellow",
-  "A_Red",
-  "B_Yellow",
-  "A_Red",
-  "B_Yellow",
-  "G_Red",
-  "B_Yellow"]
-p board.play_game(game)
-board.winner
-p board
+# p board = Connect_Four.new
+# # board.drop_checker("A_Red")
+# # p board.board
+# game =   ["A_Red",
+#   "B_Yellow",
+#   "A_Red",
+#   "B_Yellow",
+#   "A_Red",
+#   "B_Yellow",
+#   "G_Red",
+#   "B_Yellow"]
+# p board.play_game(game)
+# board.winner
 # p board
-# p board.create_row(0)
-# p board.check_rows
+# # p board
+# # p board.create_row(0)
+# # p board.check_rows
