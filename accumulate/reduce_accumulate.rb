7cd4dac1b17a187124b792e412794bd796ff52c8
Gabriel Osborne <osbornegabriel@gmail.com>
2019-03-23 12:59:10 -0400

    Add error handling, pass all provided error tests

--- accumulate/reduce_accumulate.rb
+++ accumulate/reduce_accumulate.rb
@@ -1,7 +1,10 @@
 module Enumerable
 
-
   def accumulate(*operation)
+    raise Error if !block_given? && ( operation.empty? || !operation.last.is_a?(Symbol) )
+    raise Error if block_given? && operation.first.is_a?(Symbol)
+    raise Error if operation.include?(nil)
+    raise Error if operation.count {|val| val.is_a?(Symbol)} > 1
     if !block_given?
       operator = operation[-1].to_s
       result = 0
@@ -13,8 +16,9 @@ module Enumerable
     end
     values = Array(self)
     i = values.size - 1
-    result = operation.first if operation.first.is_a?(Array)
+    result = operation.first if operation.first.is_a?(Array) || operation.first.is_a?(Hash)
     result << self[0] if operation.first.is_a?(Array)
+    result = yield(result, values[0]) if operation.first.is_a?(Hash)
     i.times do |i|
       index = i + 1
       result ||= values[i]
