c93180333bada9a477e8101e387973f0e0e85e31
Gabriel Osborne <osbornegabriel@gmail.com>
2019-03-23 12:08:24 -0400

    Accept yield blocks

--- accumulate/reduce_accumulate.rb
+++ accumulate/reduce_accumulate.rb
@@ -1,6 +1,8 @@
 module Enumerable
+
+
   def accumulate(*operation)
-    if operation.size > 0
+    if !block_given?
       operator = operation[-1].to_s
       result = 0
       result = operation[0] if operation[0].is_a?(Integer)
@@ -9,5 +11,14 @@ module Enumerable
       end
       return result
     end
+    values = Array(self)
+    i = values.size - 1
+    i.times do |i|
+      index = i + 1
+      result ||= values[i]
+      result = yield(result, values[index])
+      result
+    end
+    result
   end
 end
